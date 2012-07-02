64a1363a8aaecb02280e44550403c81064fa3e5f
WalkerS <walker.sampson@gmail.com>
2012-07-02 16:58:58 -0500

    https://github.com/WalkerS/flickr-titan

--- flickr-titan.py
+++ flickr-titan.py
@@ -2,14 +2,13 @@
 # http://eli.thegreenplace.net/2012/03/15/processing-xml-in-python-with-elementtree/
 # http://www.doughellmann.com/PyMOTW/xml/etree/ElementTree/parse.html
 
+## Run batch search-and-replace on photo titles and descriptions, by set number
+
 import flickrapi
 try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
-
-
-## Run batch search-and-replace on photo titles and descriptions, by set number
 
 api_key = 'API_KEY'
 api_secret = 'API_SECRET'
@@ -31,20 +30,18 @@ for node in set_tree.iter('photo'):																# grab all photo ids in set
 
 for single_id in photo_ids:																		# iterate over id list
 	
-	p = flickr.photos_getInfo(photo_id=single_id, api_key='TWEAK_ME')		# get each photo
+	photo = flickr.photos_getInfo(photo_id=single_id, api_key='TWEAK_ME')			# get each photo
 	
-	p_tree = ET.ElementTree(p) 																	# load into elementtree object
+	photo_tree = ET.ElementTree(photo) 															# load into elementtree object
 
 	print single_id																					# for user feedback
 	
-	for node in p_tree.iter('title'):															# get title
+	for node in photo_tree.iter('title'):														# get title
 		title = node.text
-	
-	for node in p_tree.iter('description'):													# get description
+	for node in photo_tree.iter('description'):												# get description
 		description = node.text	
 	
-	set_title       = title	
-	set_description = description.replace('TARGET_STRING', 'NEW_STRING')				# insert target and replacement values
-																											# could modify title and other values as well
-	
+	set_title       = title.replace('TARGET_STRING', 'NEW_STRING')						# insert target and replacement values
+	set_description = description.replace('TARGET_STRING', 'NEW_STRING')				
+																											
 	flickr.photos_setMeta(api_key='API_KEY', photo_id=single_id, title=set_title, description=set_description)
